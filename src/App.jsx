5acb764d482b7f4a02e41e59a6a6acd994c215aa
Leonardo Lima <leonardolima1356@gmail.com>
2025-04-01 16:51:48 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.module.css'
+import { Btn } from './components/btn'
+import { Menu } from './components/menu'
+
+function App() {
+    
+    return(
+      <>
+      <Menu s1='batata' s2='morango' s3='mandioca'/>
+        <main>
+          <section id='s1'>
+            primeira <br />
+            <Btn text='batata' func='#s2'/>
+            </section>
+          <section id='s2'>
+            segunda <br />
+            <Btn text='morango' func='#s3'/>
+            </section>
+          <section id='s3'>
+            terceira <br />
+            <Btn text='mandioca' func='#s1'/>
+            </section>
+        </main>
+      </>
+    )
+}
+
+export default App
